a9d98095eb2b38e8b2d94f59512feaacb8b566ca
unknown <mukherjeerivu7@gmail.com>
2020-07-25 21:29:33 +0530

    Updates

---
+++ precache-manifest.e9557d9d05b05891d5e5860a8d7ad9a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e4ed168a0eddc3f4d1c4e8cb11cec5",
+    "url": "/react-color-demo/index.html"
+  },
+  {
+    "revision": "3b4f8c1a619c3f408cb1",
+    "url": "/react-color-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "072bdd38b55ab43bc235",
+    "url": "/react-color-demo/static/js/2.6294a9d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-color-demo/static/js/2.6294a9d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4f8c1a619c3f408cb1",
+    "url": "/react-color-demo/static/js/main.dc2cad3e.chunk.js"
+  },
+  {
+    "revision": "d14aa75e60abef9b9dc2",
+    "url": "/react-color-demo/static/js/runtime-main.fc9a0626.js"
+  }
+]);
